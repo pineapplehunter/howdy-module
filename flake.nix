e83f41f74bef08dd89421627c053e4653a4be973
Shogo Takata <peshogo@gmail.com>
2025-05-26 11:10:39 +0900

    add default

--- flake.nix
+++ flake.nix
@@ -4,15 +4,19 @@
   outputs =
     { self }:
     {
+      nixosModules.default = self.nixosModules.howdy;
       nixosModules.howdy =
-        { lib, config }:
+        { ... }:
         {
           imports = [
             ./module/howdy.nix
             ./module/linux-enable-ir-emitter.nix
             ./module/security.nix
           ];
+
+          nixpkgs.overlays = [ self.overlays.default ];
         };
+
       overlays.default = self.overlays.howdy;
       overlays.howdy = final: prev: {
         linux-enable-ir-emitter = final.callPackage ./linux-enable-ir-emitter.nix { };
