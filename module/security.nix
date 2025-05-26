bd567c3e6671b102cc7d274bed80a832cdf289c9
Shogo Takata <peshogo@gmail.com>
2025-05-26 11:18:13 +0900

    make list

--- module/security.nix
+++ module/security.nix
@@ -1,22 +1,11 @@
-{ lib, config, ... }:
-let
-  cfg = config.howdy-module.security.pam.services.howdyAuth;
-in
+{ config, ... }:
 {
-  options.howdy-module.security.pam.services.howdyAuth = lib.mkOption {
-    default = config.services.howdy.enable;
-    defaultText = lib.literalExpression "config.services.howdy.enable";
-    type = lib.types.bool;
-    description = ''
-      If set, IR camera will be used (if it exists and your
-      facial models are enrolled).
-    '';
-  };
-
   config.security.pam.services.sudo.rules.auth = {
-    name = "howdy";
-    enable = cfg.howdyAuth;
-    control = "sufficient";
-    modulePath = "${config.services.howdy.package}/lib/security/pam_howdy.so";
+    howdy = {
+      enable = true;
+      control = "sufficient";
+      modulePath = "${config.services.howdy.package}/lib/security/pam_howdy.so";
+      order = config.security.pam.services.sudo.rules.auth.unix.order - 10;
+    };
   };
 }
