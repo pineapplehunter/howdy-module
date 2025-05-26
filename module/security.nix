98aef680518eb7d1e872a50bb938748bf959d2c8
Shogo Takata <peshogo@gmail.com>
2025-05-26 13:27:17 +0900

    more uses

--- module/security.nix
+++ module/security.nix
@@ -8,4 +8,21 @@
       order = config.security.pam.services.sudo.rules.auth.unix.order - 500;
     };
   };
+
+  config.security.pam.services.sudo.rules.auth = {
+    howdy = {
+      enable = true;
+      control = "sufficient";
+      modulePath = "${config.services.howdy.package}/lib/security/pam_howdy.so";
+      order = config.security.pam.services.sudo.rules.auth.unix.order - 500;
+    };
+  };
+  config.security.pam.services.polkit-1.rules.auth = {
+    howdy = {
+      enable = true;
+      control = "sufficient";
+      modulePath = "${config.services.howdy.package}/lib/security/pam_howdy.so";
+      order = config.security.pam.services.sudo.rules.auth.unix.order - 500;
+    };
+  };
 }
