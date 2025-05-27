1c586b146646dd111f97e2121fbf894e0071220c
Shogo Takata <peshogo@gmail.com>
2025-05-27 16:08:13 +0900

    refactor

--- flake.nix
+++ flake.nix
@@ -18,9 +18,11 @@
         };
 
       overlays.default = self.overlays.howdy;
-      overlays.howdy = final: prev: {
-        linux-enable-ir-emitter = final.callPackage ./linux-enable-ir-emitter.nix { };
-        howdy = final.callPackage ./howdy/package.nix { };
-      };
+      overlays.howdy =
+        final: prev:
+        prev.lib.packagesFromDirectoryRecursive {
+          inherit (final) callPackage;
+          directory = ./pkgs;
+        };
     };
 }
